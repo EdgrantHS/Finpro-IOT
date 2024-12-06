5a6ddae990f37921bf406efc2282635d32e14a77
EdgrantHS <edgrant.h@gmail.com>
2024-12-06 23:55:22 +0700

    init edgrant branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+// #include <BlynkSimpleEsp32.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
@@ -9,17 +9,17 @@
 #include <freertos/semphr.h>
 
 /* Fill-in information from Blynk Device Info here */
-#define BLYNK_TEMPLATE_ID ""
-#define BLYNK_TEMPLATE_NAME ""
-#define BLYNK_AUTH_TOKEN ""
+// #define BLYNK_TEMPLATE_ID ""
+// #define BLYNK_TEMPLATE_NAME ""
+// #define BLYNK_AUTH_TOKEN ""
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
 
 void resetAllValues();
-void remoteControlTask(void *pvParameters); // Kirim queue kykny
+void remoteControlTask(void *pvParameters);    // Kirim queue kykny
 void automaticControlTask(void *pvParameters); // kirim queue kykny
-void moveMotorTask(void *pvParameters); // pake queue sama mutex
+void moveMotorTask(void *pvParameters);        // pake queue sama mutex
 void sendDHTDataTask(void *pvParameters);
 void readSensorTask(void *pvParameters); // IR dan ultrasonik
 
@@ -28,19 +28,19 @@ void readSensorTask(void *pvParameters); // IR dan ultrasonik
 char ssid[] = "";
 char pass[] = "";
 
-// Remote Control 1, di sini set global variabel aja
-BLYNK_WRITE(V1)
-{
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+// // Remote Control 1, di sini set global variabel aja
+// BLYNK_WRITE(V1)
+// {
+//   int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
 
-}
+// }
 
-// Remote Control 2, di sini set global variabel aja
-BLYNK_WRITE(V2)
-{
-  int time = param.asInt(); // assigning incoming value from pin V2 to a variable
+// // Remote Control 2, di sini set global variabel aja
+// BLYNK_WRITE(V2)
+// {
+//   int time = param.asInt(); // assigning incoming value from pin V2 to a variable
 
-}
+// }
 
 void setup()
 {
@@ -48,13 +48,13 @@ void setup()
   Serial.begin(115200);
 
   // Blynk
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  resetAllValues();
+  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // resetAllValues();
 
   // FreeRTOS
 }
 
 void loop()
 {
-  Blynk.run();
+  // Blynk.run();
 }
